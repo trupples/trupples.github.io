67ac569741f89ce575eedb448cd0c053eb9d788b
ianiD <123ioandragomir123@gmail.com>
2014-12-29 17:23:56 +0200

    mADE DYNAMIC PAGES WORK...
    kinda

--- js/PAGE.js
+++ js/PAGE.js
@@ -1,1 +1,7 @@
 var PAGE = {};
+PAGE.pages = ["blog.html", "home.html", "games/index.html"];
+PAGE.initPage = function(p){
+	document.getElementById("0").style.fontSize = document.getElementById("1").style.fontSize = document.getElementById("2").style.fontSize = "50%";
+	document.getElementById(p).style.fontSize = "100%";
+	document.getElementById("pageContents").src = PAGE.pages[p];
+}
