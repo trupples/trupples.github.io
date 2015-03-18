f193e63422352f68f2c66e0ad2b65b4a0c38d791
ianiD <123ioandragomir123@gmail.com>
2015-03-18 22:04:22 +0200

    aha

--- TutoAndrei/res/entoro.js
+++ TutoAndrei/res/entoro.js
@@ -1,16 +1,20 @@
 window.requestAnimFrame = (function(){return  window.requestAnimationFrame||window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame||function(callback){window.setTimeout(callback, 1000 / 60);};})();
-var canv, ctx, interval, startupCode="", repeatCode="",mouse={x:0,y:0}
+var canv, ctx, interval, startupCode="", repeatCode="",mouse={x:0,y:0},imgBank={}
 function load(){
-	canv=document.getElementById('canv');
-	ctx=canv.getContext('2d');
+	canv=document.getElementById('canv')
+	ctx=canv.getContext('2d')
 	document.body.onerror=function(errorMsg, url, lineNumber){
-		echo(errorMsg+" line:"+lineNumber);
+		echo(errorMsg+" line:"+lineNumber)
 		clearInterval(interval)
 	}
 }
-
+function loadImg(name,url){
+	var img = new Image()
+	img.src=url
+	img.onload=function(){imgBank[name]=this}
+}
 function punct(x,y){
-	ctx.fillRect(x, y, 1, 1);
+	ctx.fillRect(x, y, 1, 1)
 }
 function dreptunghi(x, y, w, h){
 	ctx.fillRect(x, y, w, h)
@@ -29,9 +33,9 @@ function sterge(){
 }
 function culoare(contur, unplere){
 	if(typeof contur !== 'undefined')
-		ctx.strokeStyle=contur;
+		ctx.strokeStyle=contur
 	if(typeof umplere !== 'undefined')
-		ctx.fillStyle=umplere;
+		ctx.fillStyle=umplere
 }
 function fundal(stil){
 	document.getElementById("canv").style.backgroundColor=stil
@@ -43,8 +47,8 @@ function cls(){
 	document.getElementById("console").innerHTML=''
 }
 function handlemousemove(event) {
-	var rect = document.getElementById("canv").getBoundingClientRect();
-	mouse.x = event.clientX-rect.left;
-	mouse.y = event.clientY-rect.top;
+	var rect = document.getElementById("canv").getBoundingClientRect()
+	mouse.x = event.clientX-rect.left
+	mouse.y = event.clientY-rect.top
 }
 
