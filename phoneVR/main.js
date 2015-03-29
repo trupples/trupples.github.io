d3ac2b993f167caea3030ead80a74299e507bc98
ianiD <123ioandragomir123@gmail.com>
2015-03-29 16:20:34 +0300

    oops

--- phoneVR/main.js
+++ phoneVR/main.js
@@ -2,18 +2,21 @@ window.requestAnimFrame = (function(){return  window.requestAnimationFrame||wind
 var vertices = {}
 var cube
 var cam = {pos: {x:0, y:0, z:0},rot: {x:0, y:0, z:0}}
+var movement = {x:0,y:0,z:0}
 load = function(){
 	ddd.ctx=document.getElementById("canvas").getContext("2d")
 	if (!window.DeviceOrientationEvent) throw "DeviceOrientationEvent not supported!";
 	else window.addEventListener('deviceorientation', function(evt){
-		cam.rot.y = evt.alpha;
-		cam.rot.x = evt.gamma;
-		cam.rot.z = evt.beta;
+		cam.rot.y = evt.alpha * Math.PI / 180;
+		cam.rot.x = evt.gamma * Math.PI / 180;
+		cam.rot.z = evt.beta * Math.PI / 180;
 	}, false);
 	if (!window.DeviceMotionEvent) 		throw "DeviceMotionEvent not supported!";
 	else window.addEventListener('devicemotion', function(evt){
-		cam.pos.x = cam.pos.x+evt.acceleration.x*Math.cos(cam.rot.y)+evt.acceleration.z*Math.sin(cam.rot.y);
-		cam.pos.z = cam.pos.z+evt.acceleration.z*Math.sin(cam.rot.y)+evt.acceleration.x*Math.cos(cam.rot.y);
+		movement.x = movement.x+evt.acceleration.y*Math.cos(cam.rot.y)+evt.acceleration.x*Math.sin(cam.rot.y);
+		movement.z = movement.z+evt.acceleration.x*Math.sin(cam.rot.y)+evt.acceleration.y*Math.cos(cam.rot.y);
+		cam.pos.x = cam.pos.x+movement.x*100;
+		cam.pos.z = cam.pos.z+movement.z*100;
 	}, false);
 	cube = ddd.loadObj("# Blender v2.72 (sub 0) OBJ File: ''\n# www.blender.org\nmtllib cube.mtl\no Cube\nv 1.000000 -1.000000 -1.000000\nv 1.000000 -1.000000 1.000000\nv -1.000000 -1.000000 1.000000\nv -1.000000 -1.000000 -1.000000\nv 1.000000 1.000000 -0.999999\nv 0.999999 1.000000 1.000001\nv -1.000000 1.000000 1.000000\nv -1.000000 1.000000 -1.000000\nusemtl Material\ns off\nf 1 2 3 4\nf 5 8 7 6\nf 1 5 6 2\nf 2 6 7 3\nf 3 7 8 4\nf 5 1 4 8\n");
 	update();
