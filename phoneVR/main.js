afdde195cf428b12deeec20d4422a695e83da5fe
ianiD <123ioandragomir123@gmail.com>
2015-03-29 16:24:17 +0300

    asdvhasgd

--- phoneVR/main.js
+++ phoneVR/main.js
@@ -15,8 +15,8 @@ load = function(){
 	else window.addEventListener('devicemotion', function(evt){
 		movement.x = movement.x+evt.acceleration.y*Math.cos(cam.rot.y)+evt.acceleration.x*Math.sin(cam.rot.y);
 		movement.z = movement.z+evt.acceleration.x*Math.sin(cam.rot.y)+evt.acceleration.y*Math.cos(cam.rot.y);
-		cam.pos.x = cam.pos.x+movement.x*100;
-		cam.pos.z = cam.pos.z+movement.z*100;
+		//cam.pos.x = cam.pos.x+movement.x*100;
+		//cam.pos.z = cam.pos.z+movement.z*100;
 	}, false);
 	cube = ddd.loadObj("# Blender v2.72 (sub 0) OBJ File: ''\n# www.blender.org\nmtllib cube.mtl\no Cube\nv 1.000000 -1.000000 -1.000000\nv 1.000000 -1.000000 1.000000\nv -1.000000 -1.000000 1.000000\nv -1.000000 -1.000000 -1.000000\nv 1.000000 1.000000 -0.999999\nv 0.999999 1.000000 1.000001\nv -1.000000 1.000000 1.000000\nv -1.000000 1.000000 -1.000000\nusemtl Material\ns off\nf 1 2 3 4\nf 5 8 7 6\nf 1 5 6 2\nf 2 6 7 3\nf 3 7 8 4\nf 5 1 4 8\n");
 	update();
