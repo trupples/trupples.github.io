47938b02990c3d7144820e531e8712ed18f933e5
ianiD <123ioandragomir123@gmail.com>
2015-03-29 18:56:05 +0300

    Revert "asdasdasd usttorevert"
    This reverts commit d0b54102c7b44379c2ebd6f7399a0ec2d26945d6.

--- phoneVR/ddd.js
+++ phoneVR/ddd.js
@@ -2,38 +2,41 @@ var ddd = {}
 ddd.ctx
 ddd.clip = {}
 ddd.clip.min = 0.3
-ddd.clip.max = 10000
-
-//PROJECTION
-ddd.project=function(vertex){
-	var _x=-vertex.x/vertex.z;
-	var _y= vertex.y/vertex.z;
-	var _z=-vertex.z;
-	return {x:_x,y:_y,z:_z};
+ddd.clip.max = 100000000
+//http://stackoverflow.com/questions/17885850/how-to-parse-a-string-containing-text-for-a-number-float-in-javascript
+function parseSentenceForNumber(sentence){
+    var matches = /(\+|-)?((\d+(\.\d+)?)|(\.\d+))/g.exec(sentence);
+    return matches;
 }
 
-//MATRICES
+//PROECTION
+ddd.project=function(x, y, z){
+	return [-x/z, y/z, -z];
+}
+
+//MATRIX SHIT, SON, MATRIX SHIT
 ddd.matrix = {}
 
 ddd.matrix.apply=function(vertex, matrix){
+	var vert = {}
 	var x = vertex.x
 	var y = vertex.y
 	var z = vertex.z
 	var w = vertex.w
-	vertex.x	= x * matrix[0][0] + y * matrix[0][1] + z * matrix[0][2] + w * matrix[0][3]
-	vertex.y	= x * matrix[1][0] + y * matrix[1][1] + z * matrix[1][2] + w * matrix[1][3]
-	vertex.z	= x * matrix[2][0] + y * matrix[2][1] + z * matrix[2][2] + w * matrix[2][3]
-	vertex.w	= x * matrix[3][0] + y * matrix[3][1] + z * matrix[3][2] + w * matrix[3][3]
-	return vertex
+	vert.x = x * matrix[0][0] + y * matrix[0][1] + z * matrix[0][2] + w * matrix[0][3]
+	vert.y = x * matrix[1][0] + y * matrix[1][1] + z * matrix[1][2] + w * matrix[1][3]
+	vert.z = x * matrix[2][0] + y * matrix[2][1] + z * matrix[2][2] + w * matrix[2][3]
+	vert.w = x * matrix[3][0] + y * matrix[3][1] + z * matrix[3][2] + w * matrix[3][3]
+	return vert
 }
 
 //TRANSFORMATION MATRICES
-ddd.matrix.translation=function(vec){
+ddd.matrix.translation=function(x, y, z){
 	var matrix = [
-		[ 1 , 0 , 0 , vec.x ],
-		[ 0 , 1 , 0 , vec.y ],
-		[ 0 , 0 , 1 , vec.z ],
-		[ 0 , 0 , 0 , vec.w ],
+		[ 1 , 0 , 0 , x ],
+		[ 0 , 1 , 0 , y ],
+		[ 0 , 0 , 1 , z ],
+		[ 0 , 0 , 0 , 1 ],
 	];
 	return matrix
 }
@@ -80,25 +83,17 @@ ddd.loadObj=function(str){
 	var lines = str.split('\n')
 	for(var line in lines){
 		if (/^\s*v/.test(lines[line])) {	//vertex
-			var vert = /(\+|-)?((\d+(\.\d+)?)|(\.\d+))/g.exec(lines[line]);
-			vert.x=parseInt(vert.x);vert.x=vert.x?vert.x:0;
-			vert.y=parseInt(vert.y);vert.y=vert.y?vert.y:0;
-			vert.z=parseInt(vert.z);vert.z=vert.z?vert.z:0;
-			vert.w=parseInt(vert.w);vert.w=vert.w?vert.w:0;
+			var vert = parseSentenceForNumber(lines[line]);
+			vert[0]=parseInt(vert[0]);vert[0]=vert[0]?vert[0]:0;
+			vert[1]=parseInt(vert[1]);vert[1]=vert[1]?vert[1]:0;
+			vert[2]=parseInt(vert[2]);vert[2]=vert[2]?vert[2]:0;
+			vert[3]=parseInt(vert[3]);vert[3]=vert[3]?vert[3]:0;
 			mesh.vertices.push(vert);
 		}
 		if(/^s*f/.test(lines[line])) {	//face
-			var face = /(\+|-)?((\d+(\.\d+)?)|(\.\d+))/g.exec(lines[line]);
+			var face = parseSentenceForNumber(lines[line]);
 			mesh.faces.push(face);
 		}
 	}
 	return mesh
-}
-
-//conversion
-ddd.vecToArr=function(vec){
-	return [vec.x,vec.y,vec.z,vec.w];
-}
-ddd.arrToVec=function(arr){
-	return {x:arr[0],y:arr[1],z:arr[2],w:arr[3]};
 }
--- phoneVR/main.js
+++ phoneVR/main.js
@@ -3,14 +3,13 @@ var vertices = {}
 var cube
 var cam = {pos: {x:0, y:0, z:0},rot: {x:0, y:0, z:0}}
 var movement = {x:0,y:0,z:0}
-
 load = function(){
 	ddd.ctx=document.getElementById("canvas").getContext("2d")
 	if (!window.DeviceOrientationEvent) throw "DeviceOrientationEvent not supported!";
 	else window.addEventListener('deviceorientation', function(evt){
 		evt.preventDefault()
-		cam.rot.x = evt.gamma * Math.PI / 180;
 		cam.rot.y = -evt.alpha * Math.PI / 180;
+		cam.rot.x = -evt.gamma * Math.PI / 180;
 		cam.rot.z = evt.beta * Math.PI / 180;
 	}, false);
 	if (!window.DeviceMotionEvent) 		throw "DeviceMotionEvent not supported!";
@@ -18,36 +17,24 @@ load = function(){
 		evt.preventDefault()
 		movement.x = evt.acceleration.y*Math.cos(cam.rot.y)+evt.acceleration.x*Math.sin(cam.rot.y);
 		movement.z = evt.acceleration.x*Math.sin(cam.rot.y)+evt.acceleration.y*Math.cos(cam.rot.y);
-		//cam.pos.x+=movement.x;
-		//cam.pos.z+=movement.z;
+		cam.pos.x = cam.pos.x+movement.x;
+		cam.pos.z = cam.pos.z+movement.z;
 	}, false);
-	cube = {}
-	cube.vertices=[ddd.arrToVec([-1,-1,-1,+1]),
-						ddd.arrToVec([-1,-1,+1,+1]),
-						ddd.arrToVec([+1,-1,-1,+1]),
-						ddd.arrToVec([+1,-1,+1,+1]),
-						ddd.arrToVec([-1,+1,-1,+1]),
-						ddd.arrToVec([-1,+1,+1,+1]),
-						ddd.arrToVec([+1,+1,-1,+1]),
-						ddd.arrToVec([+1,+1,+1,+1])];
+	cube = ddd.loadObj("# Blender v2.72 (sub 0) OBJ File: ''\n# www.blender.org\nmtllib cube.mtl\no Cube\nv 1.000000 -1.000000 -1.000000\nv 1.000000 -1.000000 1.000000\nv -1.000000 -1.000000 1.000000\nv -1.000000 -1.000000 -1.000000\nv 1.000000 1.000000 -0.999999\nv 0.999999 1.000000 1.000001\nv -1.000000 1.000000 1.000000\nv -1.000000 1.000000 -1.000000\nusemtl Material\ns off\nf 1 2 3 4\nf 5 8 7 6\nf 1 5 6 2\nf 2 6 7 3\nf 3 7 8 4\nf 5 1 4 8\n");
 	update();
 }
-renderMesh=function(mesh){
-	var copy = Array.slice(mesh.vertices);
-	for(var v of copy){
-		console.log(v)
-		v = ddd.matrix.apply(v, ddd.matrix.translation(cam.pos));
-		v = ddd.matrix.apply(v, ddd.matrix.rotationY(cam.rot.y));
-		v = ddd.matrix.apply(v, ddd.matrix.rotationX(cam.rot.x));
-		console.log(v)
-		var c = ddd.project(v);
-		console.log(c)
-		throw "";
-		x = c.x * 400 + 400;
-		y = c.y * 300 + 400;
-		z = c.z;
+function renderMesh(mesh){
+	for(var v in mesh.vertices){
+		var vertex = {x:mesh.vertices[v][0]*100, y:mesh.vertices[v][1]*100, z:mesh.vertices[v][2]*100, w:mesh.vertices[v][3]*100}
+		vertex = ddd.matrix.apply(vertex, ddd.matrix.translation(cam.pos.x, cam.pos.y, cam.pos.z))
+		vertex = ddd.matrix.apply(vertex, ddd.matrix.rotationY(cam.rot.y))
+		vertex = ddd.matrix.apply(vertex, ddd.matrix.rotationX(cam.rot.x))
+		var c = ddd.project(vertex.x, vertex.y, vertex.z)
+		x = c[0] * 400 + 400
+		y = c[1] * 300 + 400
+		z = c[2]
 		if (z > ddd.clip.min && z < ddd.clip.max){
-			ddd.ctx.fillRect(x,y,100,100);
+			ddd.ctx.fillRect(x,y,10000/z,10000/z);
 			ddd.ctx.fillText((v+" -> ["+x+","+y+","+z+"]"),x,y)
 		}
 	}
