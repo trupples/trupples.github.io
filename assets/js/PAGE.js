eb532ed6d1b59e8887173526291b3b8ca3f8f8c8
Dragomir Ioan <123ioandragomir123@gmail.com>
2015-12-25 18:54:28 +0200

    blog stuff

--- assets/js/PAGE.js
+++ assets/js/PAGE.js
@@ -5,3 +5,10 @@ PAGE.initPage = function(p) {
 	document.getElementById(p).style.fontSize = "100%";
 	document.getElementById("pageContents").src = PAGE.pages[p];
 }
+
+window.parent.addEventListener('message', function (e) {
+	if(e.data == "try_resize") {
+		document.getElementById("pageContents").style.height = 0;
+		document.getElementById("pageContents").style.height = document.getElementById("pageContents").contentWindow.document.body.scrollHeight + 'px';
+	}
+}, false);
