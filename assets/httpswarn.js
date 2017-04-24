7acaee53667fbbc14a2a614b43ef1f728f8534c7
Dragomir Ioan <123ioandragomir123@gmail.com>
2017-04-24 13:20:34 +0300

    Added a page template and the non-https warning

--- assets/httpswarn.js
+++ assets/httpswarn.js
@@ -1,8 +1,14 @@
 "use strict";
 
 if(location.protocol != "https:") {
-	// create box
 	let box = document.createElement("div");
-	box.innerHTML = "<h2>Oops!</h2><br/>Looks like you're not using https. You should really click this alert box to switch to https (more secure + some code demos don't work without it).";
+	box.innerHTML = "<h2>Oops, you're not using https!</h2>" +
+					"You should really click this alert box to switch over to https (more secure + some code demos don't work without it).";
+	box.id = "https-warning";
+	box.classList.add("center-column");
+	box.addEventListener("click", () => {
+		location.href = 'https:' + location.href.substring(location.protocol.length);
+	});
 	
+	document.body.insertBefore(box, document.getElementById("content"));
 }
